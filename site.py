7dd3bb2a8e5b639abcc8579d367db64f8b41eecd
Emir Demic <57667464+emirdemic@users.noreply.github.com>
2020-06-13 12:24:04 +0200

    Add files via upload

---
+++ site.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd 
+import numpy as np 
+
+def site():
+    st.title('Titanic: Machine Learning from Disaster')
+    st.markdown('#### Predicting whether a particular passenger survived the Titanic sinking or not.')
+    st.sidebar.title('Options')
+
+
+site()
